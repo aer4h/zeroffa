59a81c597b1b1d2085ea2f49efcb1da6fa482d23
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2024-05-07 20:12:49 +0800

    Update and rename universal.lua to lock.lua

---
+++ lock.lua
@@ -0,0 +1,185 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local nigga = Instance.new("ScreenGui")
+local nigger = Instance.new("Frame")
+local niggaer = Instance.new("TextLabel")
+local nigger1 = Instance.new("TextButton")
+local nigger2 = Instance.new("TextButton")
+local nigger3 = Instance.new("TextButton")
+local NIGGERPASS = Instance.new("ImageLabel")
+local NIGGER = Instance.new("TextLabel")
+local NIGGER_2 = Instance.new("TextLabel")
+local NIGGER_3 = Instance.new("TextLabel")
+local NIGGER_4 = Instance.new("TextBox")
+
+--Properties:
+
+nigga.Name = "nigga"
+nigga.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+nigga.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+nigger.Name = "nigger"
+nigger.Parent = nigga
+nigger.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+nigger.BorderColor3 = Color3.fromRGB(0, 0, 0)
+nigger.BorderSizePixel = 0
+nigger.Position = UDim2.new(0.378033966, 0, 0.502876878, 0)
+nigger.Size = UDim2.new(0, 593, 0, 141)
+
+niggaer.Name = "niggaer"
+niggaer.Parent = nigger
+niggaer.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+niggaer.BackgroundTransparency = 1.000
+niggaer.BorderColor3 = Color3.fromRGB(18, 18, 18)
+niggaer.BorderSizePixel = 0
+niggaer.Position = UDim2.new(0.151046678, 0, -0.01005565, 0)
+niggaer.Size = UDim2.new(0, 424, 0, 78)
+niggaer.Font = Enum.Font.SourceSans
+niggaer.Text = "goofy skibidi sigma lock"
+niggaer.TextColor3 = Color3.fromRGB(0, 0, 0)
+niggaer.TextScaled = true
+niggaer.TextSize = 14.000
+niggaer.TextWrapped = true
+
+nigger1.Name = "nigger1"
+nigger1.Parent = nigger
+nigger1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+nigger1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+nigger1.BorderSizePixel = 0
+nigger1.Position = UDim2.new(0.0623946041, 0, 0.546099305, 0)
+nigger1.Size = UDim2.new(0, 157, 0, 50)
+nigger1.Font = Enum.Font.SourceSans
+nigger1.Text = "lock: 😎🔥🔥🔥😨🤓🤷‍🥶"
+nigger1.TextColor3 = Color3.fromRGB(0, 0, 0)
+nigger1.TextScaled = true
+nigger1.TextSize = 14.000
+nigger1.TextWrapped = true
+nigger1.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/zackmackburnger/2024-exe-test/main/freeopensourceaimbot.lua'),true))()
+end)
+
+nigger2.Name = "nigger2"
+nigger2.Parent = nigger
+nigger2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+nigger2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+nigger2.BorderSizePixel = 0
+nigger2.Position = UDim2.new(0.362990588, 0, 0.546099305, 0)
+nigger2.Size = UDim2.new(0, 157, 0, 50)
+nigger2.Font = Enum.Font.SourceSans
+nigger2.Text = "ctrl tp: 😊😍😍😍🐰🥶🔥😎😎"
+nigger2.TextColor3 = Color3.fromRGB(0, 0, 0)
+nigger2.TextScaled = true
+nigger2.TextSize = 14.000
+nigger2.TextWrapped = true
+nigger2.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/s9RU0AeX'),true))()
+end)
+
+
+nigger3.Name = "nigger3"
+nigger3.Parent = nigger
+nigger3.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+nigger3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+nigger3.BorderSizePixel = 0
+nigger3.Position = UDim2.new(0.681708992, 0, 0.546099305, 0)
+nigger3.Size = UDim2.new(0, 157, 0, 50)
+nigger3.Font = Enum.Font.SourceSans
+nigger3.Text = "F9: 🤓🤓💀💀😔🙏😡😡😡"
+nigger3.TextColor3 = Color3.fromRGB(0, 0, 0)
+nigger3.TextScaled = true
+nigger3.TextSize = 14.000
+nigger3.TextWrapped = true
+nigger3.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/zackmackburnger/wholerepositoryforprintlmao/main/lol/skibidi.lua'),true))()
+end)
+
+NIGGERPASS.Name = "NIGGERPASS"
+NIGGERPASS.Parent = nigger
+NIGGERPASS.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+NIGGERPASS.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NIGGERPASS.BorderSizePixel = 0
+NIGGERPASS.Position = UDim2.new(-0.399662733, 0, -1.2553196, 0)
+NIGGERPASS.Size = UDim2.new(0, 915, 0, 349)
+NIGGERPASS.Visible = false
+NIGGERPASS.Image = "rbxassetid://17415295417"
+
+NIGGER.Name = "NIGGER"
+NIGGER.Parent = NIGGERPASS
+NIGGER.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NIGGER.BackgroundTransparency = 1.000
+NIGGER.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER.BorderSizePixel = 0
+NIGGER.Position = UDim2.new(0.261202395, 0, 0.338108897, 0)
+NIGGER.Size = UDim2.new(0, 479, 0, 111)
+NIGGER.Font = Enum.Font.SourceSans
+NIGGER.Text = "You, my nigga now have unlimited uses of the NIGGA/NIGGER Word, Please express your emotions thru the text box nigger."
+NIGGER.TextColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER.TextScaled = true
+NIGGER.TextSize = 14.000
+NIGGER.TextWrapped = true
+
+NIGGER_2.Name = "NIGGER"
+NIGGER_2.Parent = NIGGERPASS
+NIGGER_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NIGGER_2.BackgroundTransparency = 1.000
+NIGGER_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER_2.BorderSizePixel = 0
+NIGGER_2.Position = UDim2.new(0.201093093, 0, 0, 0)
+NIGGER_2.Size = UDim2.new(0, 589, 0, 108)
+NIGGER_2.Font = Enum.Font.SourceSansBold
+NIGGER_2.Text = "THE NIGGER/NIGGA WORD PASS"
+NIGGER_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER_2.TextScaled = true
+NIGGER_2.TextSize = 14.000
+NIGGER_2.TextWrapped = true
+
+NIGGER_3.Name = "NIGGER"
+NIGGER_3.Parent = NIGGERPASS
+NIGGER_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NIGGER_3.BackgroundTransparency = 1.000
+NIGGER_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER_3.BorderSizePixel = 0
+NIGGER_3.Position = UDim2.new(0.249180391, 0, 0.727793872, 0)
+NIGGER_3.Size = UDim2.new(0, 500, 0, 94)
+NIGGER_3.Font = Enum.Font.SourceSansBold
+NIGGER_3.Text = "~lil nigga wait 10 seconds and this frame will disapepar, this is to make sure the frame fully loads nigger~"
+NIGGER_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER_3.TextScaled = true
+NIGGER_3.TextSize = 14.000
+NIGGER_3.TextWrapped = true
+
+NIGGER_4.Name = "NIGGER"
+NIGGER_4.Parent = NIGGERPASS
+NIGGER_4.BackgroundColor3 = Color3.fromRGB(62, 62, 62)
+NIGGER_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER_4.BorderSizePixel = 0
+NIGGER_4.Position = UDim2.new(0.0185792353, 0, 0.100287057, 0)
+NIGGER_4.Size = UDim2.new(0, 180, 0, 193)
+NIGGER_4.Font = Enum.Font.SourceSans
+NIGGER_4.PlaceholderColor3 = Color3.fromRGB(255, 255, 255)
+NIGGER_4.PlaceholderText = "EXAMPLE: I LOVE YOU SO MUCH MY NIGGA THANK YOU"
+NIGGER_4.Text = ""
+NIGGER_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+NIGGER_4.TextSize = 14.000
+NIGGER_4.TextWrapped = true
+
+-- Scripts:
+
+local function TQXWVFU_fake_script() -- NIGGERPASS.LocalScript 
+	local script = Instance.new('LocalScript', NIGGERPASS)
+
+	script.Parent.Visible = true wait (10) script.Parent.Visible = false
+end
+coroutine.wrap(TQXWVFU_fake_script)()
+local function LTAM_fake_script() -- nigga.Drag 
+	local script = Instance.new('LocalScript', nigga)
+
+	frame = script.Parent.nigger
+	frame.Draggable = true
+	frame.Selectable = true
+	frame.Active = true
+end
+coroutine.wrap(LTAM_fake_script)()
